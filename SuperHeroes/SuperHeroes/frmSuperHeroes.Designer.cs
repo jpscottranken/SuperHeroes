b7a249d1e417056fa575ba24359f7ff6d520db7c
Jeff Scott <jpscott@ranken.edu>
2023-06-20 15:13:06 -0500

    SuperHeroes Only - 6/20/2023

---
+++ SuperHeroes/SuperHeroes/frmSuperHeroes.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace SuperHeroes
+{
+    partial class frmSuperHeroes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblURL = new System.Windows.Forms.Label();
+            this.lblBio = new System.Windows.Forms.Label();
+            this.pbSuperHeroImage = new System.Windows.Forms.PictureBox();
+            this.cboSuperHero = new System.Windows.Forms.ComboBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblAlterEgo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSuperHeroImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnClear
+            // 
+            this.btnClear.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClear.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(57, 743);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(255, 52);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(445, 743);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(255, 52);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblURL
+            // 
+            this.lblURL.BackColor = System.Drawing.Color.Fuchsia;
+            this.lblURL.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblURL.ForeColor = System.Drawing.Color.White;
+            this.lblURL.Location = new System.Drawing.Point(54, 675);
+            this.lblURL.Name = "lblURL";
+            this.lblURL.Size = new System.Drawing.Size(646, 47);
+            this.lblURL.TabIndex = 4;
+            // 
+            // lblBio
+            // 
+            this.lblBio.BackColor = System.Drawing.Color.Fuchsia;
+            this.lblBio.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBio.ForeColor = System.Drawing.Color.White;
+            this.lblBio.Location = new System.Drawing.Point(57, 138);
+            this.lblBio.Name = "lblBio";
+            this.lblBio.Size = new System.Drawing.Size(528, 516);
+            this.lblBio.TabIndex = 3;
+            // 
+            // pbSuperHeroImage
+            // 
+            this.pbSuperHeroImage.Location = new System.Drawing.Point(624, 138);
+            this.pbSuperHeroImage.Name = "pbSuperHeroImage";
+            this.pbSuperHeroImage.Size = new System.Drawing.Size(519, 516);
+            this.pbSuperHeroImage.TabIndex = 4;
+            this.pbSuperHeroImage.TabStop = false;
+            // 
+            // cboSuperHero
+            // 
+            this.cboSuperHero.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboSuperHero.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboSuperHero.FormattingEnabled = true;
+            this.cboSuperHero.Location = new System.Drawing.Point(64, 12);
+            this.cboSuperHero.Name = "cboSuperHero";
+            this.cboSuperHero.Size = new System.Drawing.Size(521, 45);
+            this.cboSuperHero.TabIndex = 0;
+            this.cboSuperHero.SelectedIndexChanged += new System.EventHandler(this.cboSuperHero_SelectedIndexChanged);
+            // 
+            // lblName
+            // 
+            this.lblName.BackColor = System.Drawing.Color.Fuchsia;
+            this.lblName.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.Color.White;
+            this.lblName.Location = new System.Drawing.Point(66, 78);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(519, 47);
+            this.lblName.TabIndex = 5;
+            // 
+            // lblAlterEgo
+            // 
+            this.lblAlterEgo.BackColor = System.Drawing.Color.Fuchsia;
+            this.lblAlterEgo.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAlterEgo.ForeColor = System.Drawing.Color.White;
+            this.lblAlterEgo.Location = new System.Drawing.Point(624, 78);
+            this.lblAlterEgo.Name = "lblAlterEgo";
+            this.lblAlterEgo.Size = new System.Drawing.Size(519, 47);
+            this.lblAlterEgo.TabIndex = 6;
+            // 
+            // frmSuperHeroes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.CancelButton = this.btnClear;
+            this.ClientSize = new System.Drawing.Size(1167, 807);
+            this.Controls.Add(this.lblAlterEgo);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.cboSuperHero);
+            this.Controls.Add(this.pbSuperHeroImage);
+            this.Controls.Add(this.lblBio);
+            this.Controls.Add(this.lblURL);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Name = "frmSuperHeroes";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmSuperHeroes_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.pbSuperHeroImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblURL;
+        private System.Windows.Forms.Label lblBio;
+        private System.Windows.Forms.PictureBox pbSuperHeroImage;
+        private System.Windows.Forms.ComboBox cboSuperHero;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblAlterEgo;
+    }
+}
+
---
+++ SuperHeroes/SuperHeroes/frmSuperHeroes.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using Image = System.Drawing.Image;
+
+/*
+ *      LB1 SuperHeroDatabase
+ *      
+ *      Create a GUI application to look up 
+ *      information about superheroes.
+ *      
+ *      1.	Program must contain information 
+ *          about at least 5 superheroes.
+ *          
+ *      2.	Dropdown must be populated 
+ *          programmatically.
+ *          
+ *      3.  When the user selects an option from 
+ *          the dropdown, display all available 
+ *          information about that superhero.
+ */
+
+namespace SuperHeroes
+{
+    public partial class frmSuperHeroes : Form
+    {
+        public frmSuperHeroes()
+        {
+            InitializeComponent();
+        }
+
+        //  Global Arrays
+        string[] superHeroNames = {
+            "Marvel SuperHeroes",
+            "Captain America",
+            "The Incredible Hulk",
+            "The Invincible Iron Man",
+            "The Mighty Thor",
+            "The Sub-Mariner"
+        };
+
+        string[] superHeroAlterEgo = {
+            "Not Applicable",
+            "Steve Rogers",
+            "Bruce Banner",
+            "Tony Stark",
+            "Thor Odinson",
+            "Prince Namor"
+        };
+
+        string[] superHeroImages = {
+            @"C:\Users\jpscott\Desktop\Ch9-11Homework\Homework9-11\SuperHeroes\images\00-all.png",
+            @"C:\Users\jpscott\Desktop\Ch9-11Homework\Homework9-11\SuperHeroes\images\01-captainamerica.png",
+            @"C:\Users\jpscott\Desktop\Ch9-11Homework\Homework9-11\SuperHeroes\images\02-hulk.png",
+            @"C:\Users\jpscott\Desktop\Ch9-11Homework\Homework9-11\SuperHeroes\images\03-ironman.png",
+            @"C:\Users\jpscott\Desktop\Ch9-11Homework\Homework9-11\SuperHeroes\images\04-thor.png",
+            @"C:\Users\jpscott\Desktop\Ch9-11Homework\Homework9-11\SuperHeroes\images\05-submariner.png"
+        };
+
+        string[] superHeroBio = {
+            "The Marvel Super Heroes is an American animated television series starring five comic book superheroes from Marvel Comics. The first TV series based on Marvel characters, it debuted in syndication on U.S. television in 1966. The segments were Captain America, The Incredible Hulk, Iron Man, The Mighty Thor and The Sub-Mariner.",
+            "Captain America was designed as a patriotic supersoldier who often fought the Axis powers of World War II. Captain America is the alter ego of Steve Rogers, a frail young artist enhanced to the peak of human perfection by an experimental super-soldier serum after joining the military to aid the United States government's efforts in World War II.",
+            "The Hulk is a green-skinned, hulking and muscular humanoid possessing a limitless degree of physical strength, and his alter ego Dr. Robert Bruce Banner, a physically weak, socially withdrawn, and emotionally reserved physicist. The two exist as independent dissociative personalities, and resent each other, following Banner's accidental exposure to gamma rays.",
+            "Iron Man is a superhero appearing in comic books published by Marvel Comics. Wealthy business magnate, playboy, philanthropist, inventor and scientist Anthony Edward Stark suffers a severe chest injury during a kidnapping. When his captors attempt to force him to build a weapon of mass destruction, he instead creates a mechanized suit.",
+            "Thor Odinson, or simply Thor, is a fictional character appearing in American comic books published by Marvel Comics. The character is based on the Norse deity of the same name, the Asgardian god of thunder whose enchanted hammer Mjolnir enables him to fly and manipulate weather.",
+            "Namor McKenzie, also known as the Sub-Mariner, is a fictional character appearing in American comic books published by Marvel Comics. The mutant son of a human sea captain and a princess of the mythical undersea kingdom of Atlantis, Namor possesses the super-strength and aquatic abilities ."
+        };
+
+        string[] superHeroURL = {
+            "https://en.wikipedia.org/wiki/The_Marvel_Super_Heroes",
+            "https://en.wikipedia.org/wiki/Captain_America",
+            "https://en.wikipedia.org/wiki/Hulk",
+            "https://en.wikipedia.org/wiki/Iron_Man",
+            "https://en.wikipedia.org/wiki/Thor_(Marvel_Comics)",
+            "https://en.wikipedia.org/wiki/Namor"
+        };
+
+        private void frmSuperHeroes_Load_1(object sender, EventArgs e)
+        {
+            //  Filling up the combo box.
+            for (int lcv = 0; lcv < superHeroNames.Length; lcv++)
+            {
+                cboSuperHero.Items.Add(superHeroNames[lcv]);
+            }
+
+            ClearAll();
+        }
+
+        private void cboSuperHero_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ShowCorrectSuperHero();
+        }
+
+        private void ShowCorrectSuperHero()
+        {
+            int si = cboSuperHero.SelectedIndex;
+
+            switch (si)
+            {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                    CreateSuperHero(si);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private void CreateSuperHero(int si)
+        {
+            pbSuperHeroImage.Image = System.Drawing.Image.FromFile(superHeroImages[si]);
+            lblName.Text = "Name: " + superHeroNames[si];
+            lblAlterEgo.Text = "Alter Ego: " + superHeroAlterEgo[si];
+            lblBio.Text = "Bio:  " + superHeroBio[si];
+            lblURL.Text = "URL:  " + superHeroURL[si];
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearAll();
+        }
+
+        private void ClearAll()
+        {
+            cboSuperHero.SelectedIndex = 0;
+            lblName.Text = "Name: " + superHeroNames[0];
+            lblBio.Text = "Bio:  " + superHeroBio[0];
+            lblURL.Text = "URL:   " + superHeroURL[0];
+            pbSuperHeroImage.Image = System.Drawing.Image.FromFile(superHeroImages[0]);
+        }
+
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            ExitProgramOrNot();
+        }
+
+        private void ExitProgramOrNot()
+        {
+            DialogResult dialog = MessageBox.Show(
+                   "Do You Really Want To Exit?",
+                   "EXIT NOW?",
+                   MessageBoxButtons.YesNo,
+                   MessageBoxIcon.Question);
+
+            if (dialog == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+    }
+}
